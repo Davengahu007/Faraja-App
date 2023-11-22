bc516679504a9e8d416f8895c7c9b270bf71e1d2
phantasythenoob <davidkingmwai@gmail.com>
2023-11-22 14:09:43 +0300

    Updates

--- app/src/main/java/com/example/faraja_app/MainActivity.kt
+++ app/src/main/java/com/example/faraja_app/MainActivity.kt
@@ -2,21 +2,30 @@ package com.example.faraja_app
 
 import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.widget.ToggleButton
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
@@ -27,9 +36,14 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.faraja_app.ui.theme.Faraja_AppTheme
@@ -51,9 +65,7 @@ fun Drawer(
             contentAlignment = Alignment.TopStart
         ) {
 
-                ExtendedFloatingActionButton(
-                    text = { Text("Menu") },
-                    icon = { Icon(Icons.Filled.Menu, contentDescription = "") },
+                FloatingActionButton(
                     onClick = {
                         scope.launch {
                             drawerState.apply {
@@ -62,7 +74,9 @@ fun Drawer(
                         }
                     },
                     modifier = Modifier.padding(16.dp)
-                )
+                ) {
+                    Icon(Icons.Filled.Menu, contentDescription = "")
+                }
             }
     }
 }
@@ -71,7 +85,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavBar()
+            HomeContainer()
         }
     }
 }
@@ -79,10 +93,72 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavBar() {
-    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-    val scope = rememberCoroutineScope()
-
     Drawer()
+}
+
+@Composable
+fun Switcher(
+    onToggle: (String) -> Unit
+) {
+    var selectedOption by remember { mutableStateOf("Communities") }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(18.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        ToggleButton(
+            text = "Communities",
+            selected = selectedOption == "Communities",
+            onToggle = {
+                selectedOption = "Communities"
+                onToggle(selectedOption)
+            }
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        ToggleButton(
+            text = "Counselors",
+            selected = selectedOption == "Counselors",
+            onToggle = {
+                selectedOption = "Counselors"
+                onToggle(selectedOption)
+            }
+        )
+
+    }
+}
+
+@Composable
+fun ToggleButton(
+    text: String,
+    selected: Boolean,
+    onToggle: () -> Unit
+) {
+    Button(
+        onClick = {
+        onToggle()
+    },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (selected) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.background
+        )
+    ) {
+        Text(text = text, color = if (selected) Color.White else MaterialTheme.colorScheme.primary)
+
+    }
+}
+
+@Composable
+fun HomeContainer() {
+    var selectedOption by remember {
+        mutableStateOf("Communities")
+    }
+    NavBar()
+    Switcher { option ->
+        selectedOption = option
+    }
 }
 
 fun onDrawerItemClicked(item: String) {
