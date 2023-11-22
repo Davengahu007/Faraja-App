591eaa34e7cceef18c5e84a4482cc7e7102c8bdd
phantasythenoob <davidkingmwai@gmail.com>
2023-11-22 15:30:56 +0300

    Updates

--- app/src/main/java/com/example/faraja_app/MainActivity.kt
+++ app/src/main/java/com/example/faraja_app/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.faraja_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,9 +13,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
@@ -24,6 +29,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -34,7 +40,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.faraja_app.ui.theme.Faraja_AppTheme
@@ -125,10 +133,26 @@ fun Switcher(
 }
 @Composable
 fun CommunityCard(community: Community) {
-    Row {
+    Row(
+        modifier = Modifier.padding(top = 85.dp, start = 15.dp)
+    ) {
+        Image(painter = painterResource(R.drawable.guitar), contentDescription = "Community profile picture", modifier = Modifier
+            .size(40.dp)
+            .clip(
+                CircleShape
+            )
+            .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Column {
-            Text(text = "Hello ${community.name}")
-            Text(text = "Hello ${community.latestMessage}")
+            Text(text = "Hello ${community.name}",
+                style = MaterialTheme.typography.titleSmall
+                )
+            Spacer(modifier = Modifier.height(3.dp))
+                Text(text = "Hello ${community.latestMessage}",
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodyMedium
+                )
         }
     }
 }
