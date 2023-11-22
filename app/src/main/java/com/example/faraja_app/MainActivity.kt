120fa511ac948b3e32e6f0f1be9025d4e4bf7825
phantasythenoob <davidkingmwai@gmail.com>
2023-11-22 14:40:56 +0300

    Updates

--- app/src/main/java/com/example/faraja_app/MainActivity.kt
+++ app/src/main/java/com/example/faraja_app/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -73,7 +75,8 @@ fun Drawer(
                             }
                         }
                     },
-                    modifier = Modifier.padding(16.dp)
+                    modifier = Modifier.padding(top = 25.dp, start = 10.dp).size(38.dp)
+
                 ) {
                     Icon(Icons.Filled.Menu, contentDescription = "")
                 }
